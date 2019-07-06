8f4bcbac11add8f53675621ffffe06a5a4987b40
Moses Lara <mlara417@gmail.com>
2019-07-06 18:45:44 -0500

    practice

--- Type conversion/type_conversion.js
+++ Type conversion/type_conversion.js
@@ -2,7 +2,36 @@ let val;
 
 //Number to string
 val = String(5);
+val = String(4*4);
+//Bool to string
+val String(true);
+//Date to string
+val = String(new Date());
+//Array to string
+val = String ([1,2,3,4]);
+
+//toString()
+val =(5).toString();
+val = (true).toString();
+
+//String to number
+val = Number('5');
+val = Number(true);
+val = Number(false);
+val = Number(null);
+val = Number("hello");
+val = Number([1,2,3]);
+
+val = parseInt('100,30');
+val = parseFloat('100,30');
+
 //Output
-console.log(val);
-console.log(typeof val);
-console.log(val.length);
+//console.log(val);
+//console.log(typeof val);
+//console.log(val.length);
+
+const val1 = String($);
+const val2 = 6;
+const sum = Number(val1 * val2);
+
+console.log(sum);

f6aa18e4fbb22c99861907179f3ed7def4aafe1c
Moses Lara <mlara417@gmail.com>
2019-07-05 22:52:00 -0500

    practicing variables - var, let, const

--- app.js
+++ app.js
@@ -1,4 +1,34 @@
-console.log("H");
-console.log(123);
-console.log(true);
-var greeting = "Hello"
+//var, let, const
+
+var name = "Moses Lara";
+console.log(name);
+name = "Hello";
+console.log(name);
+
+//Init var
+var greeting;
+console.log(greeting);
+greeting = "Yo";
+console.log(greeting);
+
+//letters, numbers, _, $
+// Cannot start with number
+
+//Multiword vars
+var firstName; //camel case
+var first_name; //underscore
+var FirstName; //Pascal caswe
+
+//LET
+let name = "Moses Lara";
+console.log(name);
+name = "Hello";
+console.log(name);
+
+//Const
+const name = "John";
+console.log(name);
+//Cannot reassign
+//Have to assign a value
+//const greeting;
+

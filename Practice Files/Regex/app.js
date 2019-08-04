5a765e7331f31d3f8b97c07b6566db70af8b055a
Moses Lara <mlara417@gmail.com>
2019-08-03 20:02:50 -0500

    RegEx charactersets, quantifiers, and grouping

--- Practice Files/Regex/app.js
+++ Practice Files/Regex/app.js
@@ -12,8 +12,25 @@ re = /^h*llo$/i; //matches any character 0 or more times (*)
 re = /gre?a?y/i;  //Optional character (?) placed after 
 re = /gre?a?y\?/i;  //Escape character(\) when trying to use as a literal instead of regex
 
+//Brackets [] - character sets
+re = /gr[ae]y/i;  //Must be an a or e
+re = /[GF]ray/;  //Must be a G or F
+re = /[^GF]ray/i;  //Match anything except a G or F
+re = /[A-Z]ray/;  //Match any uppercase letter
+re = /[a-z]ray/;  //Match any lowercase letter
+re = /[A-Za-z]ray/;  //Match any letter
+re = /[0-9]ray/;  //Match any digit
+
+//Braces {} - Quantifiers
+re = /Hel{2}o/i; //Must occur exactly (m) amount of times
+re = /Hel{2,4}o/i; //Must occur between range m - n (m, n) amount of times
+re = /Hel{2,}o/i; //Must occur at least (m) times
+
+//parentheses () - Grouping
+re = /^([0-9]x){3}$/; //Must be exactly 3x 3 times
+
 //String to match
-const str = 'Gray?';
+const str = 'Hello';
 
 //Log results
 const result = re.exec(str);
